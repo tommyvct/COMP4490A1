9b7e3f2cd473aea640e31308f02404f72eb88aa7
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2022-02-24 13:57:17 -0600

    init

--- src/fshader8cube.glsl
+++
@@ -1,9 +0,0 @@
-#version 150
-
-flat in vec4 f_colour;
-out vec4 out_colour;
-
-void main() 
-{
-  out_colour = f_colour;
-}
--- src/fshader8height.glsl
+++
@@ -1,8 +0,0 @@
-#version 150
-
-out vec4 out_colour;
-
-void main() 
-{
-  out_colour = vec4(0,0,0,1);
-}
